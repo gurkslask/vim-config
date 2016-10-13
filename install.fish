2e7998ccd0ec4fa9b5307f8542f5262e2d489e1b
Alexander Svensson <gurkslask@gmail.com>
2016-10-13 01:51:01 -0700

    added install script

---
+++ install.fish
@@ -0,0 +1,11 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone https://github.com/scrooloose/nerdtree.git  ~/.vim/bundle/nerdtree
+git clone  https://github.com/sjl/gundo.vim.git ~/.vim/bundle/gundo
+git clone  https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+git clone  https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi
+git clone   https://github.com/Shougo/neocomplete.vim.git ~/.vim/bundle/neocomplete
+
+git clone https://github.com/sjl/badwolf.git ~/.vim/colors/badwolf
+cp ~/.vim/colors/badwolf/colors ~/.vim/colors/ -r
